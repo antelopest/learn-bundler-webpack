9501fe436de48eec4822b0a69f4b1a5684d774b7
antelopest <antelopest@gmail.com>
2020-11-29 14:16:18 +0300

    Added files in root catalog. Overview Webpack.

---
+++ 01.overview.js
@@ -0,0 +1,28 @@
+/*
+* Overview Webpack version 4
+*
+* Plan:
+* What is webpack?
+* Diff Bundler and Task manager
+* Webpack and other solutions
+* Dependency graph
+* First configuration on webpack
+* */
+
+/*
+* What is Webpack?
+*
+* Inventor - Tobias Koppers
+*
+*
+* Task Runner
+* Gulp, Grunt - task runner
+* SASS -> CSS
+* TPL -> HTML
+* Babel -> JS
+* Minification files
+* and and ...
+*
+* Sequential execution of tasks.
+*
+* */
